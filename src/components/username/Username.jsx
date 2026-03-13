c2984f3b86493aed04e75813bc6cad2fcaf79f83
MMDavidoff <myron.davv@gmail.com>
2026-03-13 17:56:06 +0400

    modal exit

--- src/components/username/Username.jsx
+++ src/components/username/Username.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Username.module.css";
 import { Link } from "react-router-dom";
+import ButtonItem from "../buttonItem/ButtonItem";
 const Username = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  }
   return (
-    <div className={classes.topSection}>
-      <div className={classes.mainUserCard}>
-        <div className={classes.userCardFlex}>
-        <img className={classes.userLogo} src="/image/userLogo.png" alt="" />
-        <Link className={classes.userCard}>Username</Link>
+    <>
+      <div className={classes.topSection}>
+        <div className={classes.mainUserCard}>
+          <div className={classes.userCardFlex} onClick={toggleModal}>
+            <img
+              className={classes.userLogo}
+              src="/image/userLogo.png"
+              alt=""
+            />
+            <Link className={classes.userCard}>Username</Link>
+          </div>
         </div>
       </div>
-    </div>
+      <div className={`${classes.exitModal} ${isOpen ? classes.active : ""}`}>
+          <ButtonItem
+            name={"Settings"}
+            logo={"/image/exit-yuvo.png"}
+            className={classes.buttonSettings}
+          />
+
+        <ButtonItem
+          name={"Log out"}
+          logo={"/image/exit-yuvo.png"}
+          className={classes.buttonExit}
+        />
+      </div>
+    </>
   );
 };
 
